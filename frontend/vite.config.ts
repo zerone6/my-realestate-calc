47fc62cc88e264166893b99e3acfbd9e97548514
zerone6 <zerone6@naver.com>
2025-06-14 01:32:14 +0900

    perfact

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/index.html', // 브라우저 자동 열기
+  }
 })
